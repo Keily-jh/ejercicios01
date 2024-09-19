0656e9ae4b886ec8046d152e1ff74d0d3e16dbd2
Keily <202411882@urp.edu.pe>
2024-09-19 19:25:20 +0000

    y

--- practicando/ejercicio_01.py
+++ practicando/ejercicio_01.py
@@ -3,21 +3,20 @@ class Vehiculo:
         self.Marca = Marca
         self.Modelo = Modelo
         self.velocidad_máxima = velocidad_máxima 
-    def detalles(self):
+    def __str__(self):
         return f'Vehiculo => Marca: {self.Marca}, Modelo: {self.Modelo}, velocidad_máxima: {self.velocidad_máxima}'
 
 class Automóvil(Vehiculo):
     def __init__(self, Marca, Modelo, velocidad_máxima, numero_puertas):
-        super().__init__(self. Marca, Modelo, velocidad_máxima)
+        super().__init__(Marca, Modelo, velocidad_máxima)
         self.numero_puertas = numero_puertas
-    def detalles(self):
-        return f'Vehiculo => Marca: {self.Marca}, Modelo: {self.Modelo}, velocidad_máxima: {self.velocidad_máxima, Número de puertas: {numero_puertas}}'
-
+    def __str__(self):
+        return f'Automóvil => Marca: {self.Marca}, Modelo: {self.Modelo}, velocidad_máxima: {self.velocidad_máxima}, numero_puertas: {self.numero_puertas}'
 class Motocicleta(Vehiculo):
     def __init__(self, Marca, Modelo, velocidad_máxima, Tipo):
-        super().__init__(self. Marca, Modelo, velocidad_máxima)
+        super().__init__(Marca, Modelo, velocidad_máxima)
         self.Tipo = Tipo
-    def detalles(self):
+    def __str__(self):
         return f'Vehiculo => Marca: {self.Marca}, Modelo: {self.Modelo}, velocidad_máxima: {self.velocidad_máxima, Tipo: {Tipo}}'
 
 
--- practicando/test/test_ejercicio_01.py
+++ practicando/test/test_ejercicio_01.py
@@ -1,13 +1,13 @@
 import unittest
 from practicando.ejercicio_01 import Vehiculo, Automóvil, Motocicleta
 
-class Testvehiculo(unittest.testcase):
+class Testvehiculo(unittest.TestCase):
     def test_propiedades_vehiculo(self):
         ve1 = Vehiculo('Porche', 'g34', 334)
         self.assertEqual(ve1.__str__(), 'Vehiculo => Marca: Porche, Modelo: g34, velocidad_máxima: 334')
-    def test_prpiedades_automovil(self):
+    def test_propiedades_automovil(self):
         auto1 = Automóvil('Jeep', 'Rubicon', 122, 4)
-        self.assertEqual(auto1.__str__(), 'Automóvil => Marca: Jeep, Modelo: Rubicon, velocidad_máxima: 122, número_puertas: 4')
+        self.assertEqual(auto1.__str__(), 'Automóvil => Marca: Jeep, Modelo: Rubicon, velocidad_máxima: 122, numero_puertas: 4')
     def test_propiedades_moto(self):
         moto1 = Motocicleta('Honda', 'civic', 78, 'Deportiva')
 
